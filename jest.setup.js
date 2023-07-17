f85fb3344c672b24cf81d256e29444dfa52b8460
Dmitrii Teriaev <mitya.teryaew@gmail.com>
2023-07-17 05:13:06 +0400

    test(*): fix index page smoke test

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom/extend-expect";
+
+// Fixes ReferenceError: TextEncoder is not defined
+import { TextEncoder, TextDecoder } from "util";
+Object.assign(global, { TextDecoder, TextEncoder });
--- src/__tests__/pages/index.test.tsx
+++ src/__tests__/pages/index.test.tsx
@@ -1,8 +1,10 @@
-import { render } from "@testing-library/react";
+import { act, render, waitFor } from "@testing-library/react";
 
 import IndexPage from "@/pages";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
 
-/** Mock Seo"s useRouter */
+/** Mock useRouter */
 jest.mock("next/router", () => ({
     useRouter() {
         return {
@@ -11,9 +13,23 @@ jest.mock("next/router", () => ({
     },
 }));
 
+const createWrapper = () => {
+    // ✅ creates a new QueryClient for each test
+    const queryClient = new QueryClient();
+    return ({ children }: { children: ReactNode }) => (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
+
 describe("Index Page", () => {
     it("renders index page", async () => {
-        const { container } = render(<IndexPage />);
+        const { container } = await act(async () =>
+            render(<IndexPage />, {
+                wrapper: createWrapper(),
+            })
+        );
 
         expect(container.firstChild?.hasChildNodes()).toBeTruthy();
     });
